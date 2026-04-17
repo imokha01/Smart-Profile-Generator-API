cfc91f2e10297e90dd88458b86894e592e324b07
Ahmed Wahab <imokha0304@gmail.com>
2026-04-17 23:47:32 +0100

    updated the app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ app.use(express.json());
 // CORS (REQUIRED)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
   next();
 });
+
 
 // HEALTH CHECK
 app.get("/", (req, res) => {
