912b9b7bb4e3de757d63ea1c9c3a68624ce715ae
Ahmed Wahab <imokha0304@gmail.com>
2026-04-17 22:45:52 +0100

    new update

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
   })
   .catch((err) => {
     console.error("DB connection error:", err);
