deed68e7e7a96c9d0f94a119139ed8da3df6789e
Ahmed Wahab <imokha0304@gmail.com>
2026-04-17 23:24:53 +0100

    updated the server.js

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
 
-    app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
   })
   .catch((err) => {
     console.error("DB connection error:", err);
